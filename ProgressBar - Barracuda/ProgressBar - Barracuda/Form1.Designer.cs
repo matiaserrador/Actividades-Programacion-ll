ef94f45e46377e4699f860c3c5ea9d582cd580be
Matias Errador <loco.matu@hotmail.com>
2022-05-21 15:34:51 -0300

    Barrita de progreso.

---
+++ ProgressBar - Barracuda/ProgressBar - Barracuda/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ProgressBar___Barracuda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barra = new System.Windows.Forms.ProgressBar();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // barra
+            // 
+            this.barra.Location = new System.Drawing.Point(12, 64);
+            this.barra.Name = "barra";
+            this.barra.Size = new System.Drawing.Size(776, 24);
+            this.barra.TabIndex = 0;
+            // 
+            // bt1
+            // 
+            this.bt1.BackColor = System.Drawing.Color.Indigo;
+            this.bt1.Location = new System.Drawing.Point(319, 242);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 23);
+            this.bt1.TabIndex = 1;
+            this.bt1.Text = "B o t ó n";
+            this.bt1.UseVisualStyleBackColor = false;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProgressBar___Barracuda.Properties.Resources.wallpapersden_com_purple_sunrise_4k_vaporwave_1920x1080;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.barra);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "B A R R A C U D A - D E - P R O G R E S O";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar barra;
+        private System.Windows.Forms.Button bt1;
+    }
+}
+
---
+++ ProgressBar - Barracuda/ProgressBar - Barracuda/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBar___Barracuda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            // Instancio el valor inicial de la barra 
+            barra.Value = 0;
+
+            //Instancia el valor máximo de la barra
+            barra.Maximum = 100;
+
+            //Instancio el valor Mininmo de la barra
+            barra.Minimum = 0;
+
+            //El "paso a paso" sera de uno en uno, dado este valor.
+            barra.Step = 1;
+
+            //Cargamos la barracuda
+            for (int i = barra.Minimum; i < barra.Maximum; i = i + barra.Step)
+            {
+                //Esto hace que la barra vaya avanzando.
+                barra.PerformStep();
+            }
+
+        }
+    }
+}
