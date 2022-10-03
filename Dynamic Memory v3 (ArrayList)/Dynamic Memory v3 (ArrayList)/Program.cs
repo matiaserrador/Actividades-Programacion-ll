b21313dee0598825a34bb47ce076e3eb73994ceb
Matias Errador <66315195+matiaserrador@users.noreply.github.com>
2022-10-03 09:13:14 -0300

    Add files via upload
    Memoria dinámica v3

---
+++ Dynamic Memory v3 (ArrayList)/Dynamic Memory v3 (ArrayList)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Dynamic_Memory_v3__ArrayList_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Frase;
+            int Numero;
+            List<string> Lista = new List<string>();
+            Frase = Console.ReadLine();
+
+
+            while (Frase != "")
+            {
+                Lista.Add(Frase);
+                Console.WriteLine("Ingrese la proxima palabra");
+                Frase = Console.ReadLine();
+            }
+            Console.WriteLine("Ingrese la posición de la palabra que desea");
+
+            Numero = Convert.ToInt32(Console.ReadLine());
+            while (Numero != 0)
+            {
+                if (Numero > Lista.Count())
+                {
+                    Console.WriteLine("No hay datos en esta posicion, ingrese otra posicion");
+                    Numero = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Numero--;
+                    Console.WriteLine(Lista[Numero]);
+                    Numero = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+
+        }
+    }
+}
