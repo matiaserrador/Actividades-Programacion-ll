a81c61fca613aa2b14373d954ef244a216178b0e
Matias Errador <66315195+matiaserrador@users.noreply.github.com>
2022-10-03 09:12:11 -0300

    Add files via upload
    Memoria dinámica v2 (Pila)

---
+++ Dynamic Memory v2 (Pila)/Dynamic Memory v2 (Pila)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Dynamic_Memory_v2__Pila_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Cola = estructura LIFO.
+            Stack pilaDeNombres = new Stack();
+            string INFORMACION;
+            pilaDeNombres.Push("1 - Matias");
+            pilaDeNombres.Push("2 - Rodrigo");
+            pilaDeNombres.Push("3 - Errador");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.ReadKey();
+                INFORMACION = (string)pilaDeNombres.Pop();
+                Console.WriteLine(INFORMACION);
+            }
+            Console.ReadKey();
+        }
+    }
+}
