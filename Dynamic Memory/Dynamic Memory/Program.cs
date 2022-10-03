467f67083692c89025fc99b1e324978e632bd226
Matias Errador <66315195+matiaserrador@users.noreply.github.com>
2022-10-03 09:10:51 -0300

    Add files via upload
    Memoria dinámica v1

---
+++ Dynamic Memory/Dynamic Memory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dynamic_Memory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Frase;
+
+            Queue<string> Frase2 = new Queue<string>();
+
+            Console.WriteLine("Escribí acá");
+            Frase = Console.ReadLine();
+
+            while (Frase != "")
+            {
+                Frase2.Enqueue(Frase);
+                Console.WriteLine("Escribí acá");
+                Frase = Console.ReadLine();
+
+            }
+            while (Frase2.Count > 0)
+            {
+                Console.WriteLine(Frase2.Dequeue());
+            }
+        }
+
+    }
+}
